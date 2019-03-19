891fc3915e85a1ba9f2b3125404e386a6064db6c
sawravchy <sawravador@gmail.com>
2019-03-20 02:33:28 +0600

    first commit

---
+++ DemoCognitiveTextApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DemoCognitiveTextApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for input text
+            Console.WriteLine("Provide the Text: ");
+            string input = Console.ReadLine();
+            //generate guid
+            string id = Guid.NewGuid().ToString();
+            //run the sentiment analysis for input value
+            TextAnalysis.RunSentimentAnalysis("en", id, input).Wait();
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
---
+++ DemoCognitiveTextApp/TestAnalysis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Text.KeyPhrase;
+using Microsoft.ProjectOxford.Text.Sentiment;
+namespace DemoCognitiveTextApp
+{
+    public class TextAnalysis
+    {
+        static readonly string apiKey = ConfigurationManager.AppSettings["TextAPIKey"];
+        public static async Task RunSentimentAnalysis(string language, string id, string input)
+        {
+            //Prepare document object
+            var doc = new SentimentDocument()
+            {
+                Id = id,
+                Text = input,
+                Language = language
+            };
+            //get the client and request handler
+            var sentimentClient = new SentimentClient(apiKey);
+            var sentimentRequest = new SentimentRequest();
+            //Add document to the request
+            sentimentRequest.Documents.Add(doc);
+
+            try
+            {
+                //call Azure service for sentiment analysis on given text
+                var response = await sentimentClient.GetSentimentAsync(sentimentRequest);
+                Console.WriteLine("**Sentiment Analysis Results** ");
+                //get the response document
+                foreach (var resdoc in response.Documents)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sentiment Score: {0}", (resdoc.Score));
+                    Console.WriteLine();
+                    Console.WriteLine("Sentiment Score in Percentage: {0}", (resdoc.Score * 100));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
